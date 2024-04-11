f63d2df21153b238a7019bb8870367f50878745f
Alan San <alansandovalt1@gmail.com>
2024-04-10 18:00:04 -0600

    Añadir archivos de configuración y código fuente

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+	  extend: {
+		fontFamily: {
+		  pirate: ["Pirata One", "cursive",],
+		},
+		colors: {
+		  primary: "var(--color-primary)",
+		  secundary: "var(--color-secundary)",
+		  
+		},
+	  },
+	},
+
+  };
+  
